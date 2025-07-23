cf912a835eda7aedff46617bfa28527ce8c9d72c
ThomasdevLC <thomas.lecam@gmail.com>
2025-07-23 15:32:23 +0200

    util validator for form

--- src/models/client.ts
+++ src/models/client.ts
@@ -10,12 +10,12 @@ export interface Client {
 }
 
 export interface ClientUpdate {
-  nom?: string
-  email?: string
-  tel?: string
-  adresse?: string
-  code_postal?: string
-  ville?: string
+  nom: string
+  email: string
+  tel: string
+  adresse: string
+  code_postal: string
+  ville: string
 }
 
 export interface ClientFilters {
